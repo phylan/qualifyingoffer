3ba3f982cb3fd9e953b56c788c9e0c7f0adfa9a1
Ryan Sommers <ryansommers@gmail.com>
2020-08-10 23:39:21 -0400

    adding API controller

---
+++ QO.API/Controllers/QualifyingOfferController.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using QO.Application.Models;
+using QO.Application.Queries;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace QO.API.Controllers
+{
+    public class QualifyingOfferController : ApiController
+    {
+        private readonly IMediator _mediator;
+
+        public QualifyingOfferController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<QualifyingOfferViewModel> Get() => await _mediator.Send(new QualifyingOfferQuery());
+    }
+}
